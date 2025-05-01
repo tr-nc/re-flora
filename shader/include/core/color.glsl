9dfc19c33463f9e7b625e945a92729560ed96665
danny <ruitian0716@gmail.com>
2025-05-01 16:34:20 +0800

    mixing voxel color and normal, figured out color space usage

--- shader/include/core/color.glsl
+++ shader/include/core/color.glsl
@@ -1,6 +1,23 @@
 #ifndef COLOR_GLSL
 #define COLOR_GLSL
 
-float lum(vec3 col) { return dot(col, vec3(0.2126, 0.7152, 0.0722)); }
+float luminance(vec3 col) { return dot(col, vec3(0.2126, 0.7152, 0.0722)); }
+
+// the following two functions are taken from:
+// https://gamedev.stackexchange.com/questions/92015/optimized-linear-to-srgb-glsl
+// when converting vec4s, the alpha channel should be kept as-is
+vec3 linear_to_srgb(vec3 c) {
+    bvec3 cutoff = lessThan(c, vec3(0.0031308));
+    vec3 higher  = vec3(1.055) * pow(c, vec3(1.0 / 2.4)) - vec3(0.055);
+    vec3 lower   = c * vec3(12.92);
+    return mix(higher, lower, cutoff);
+}
+
+vec3 srgb_to_linear(vec3 c) {
+    bvec3 cutoff = lessThan(c, vec3(0.04045));
+    vec3 higher  = pow((c + vec3(0.055)) / vec3(1.055), vec3(2.4));
+    vec3 lower   = c / vec3(12.92);
+    return mix(higher, lower, cutoff);
+}
 
 #endif // COLOR_GLSL
--- shader/include/voxel_types.glsl
+++ shader/include/voxel_types.glsl
@@ -1,13 +1,26 @@
 #ifndef VOXEL_TYPE_GLSL
 #define VOXEL_TYPE_GLSL
 
-const uint VOXEL_TYPE_EMPTY = 0;
-const uint VOXEL_TYPE_DIRT  = 1;
-const uint VOXEL_TYPE_ROCK  = 2;
-const uint VOXEL_TYPE_SAND  = 3;
-const uint VOXEL_TYPE_GRASS = 4;
-const uint VOXEL_TYPE_LEAF  = 5;
+const uint VOXEL_TYPE_EMPTY      = 0;
+const uint VOXEL_TYPE_GRASS_LAND = 1;
+const uint VOXEL_TYPE_LEAF       = 2;
+const uint VOXEL_TYPE_CHUNK      = 3;
 
-const uint VOXEL_TYPE_MAX = 5;
+// const uint VOXEL_TYPE_MAX = 3;
+
+// https://colorhunt.co/palette/5b913b77b254ffe8b6d99d81
+// all color pickers are in sRGB space for human preservation, but lighting calculations are done in
+// linear space
+vec3 voxel_srgb_color_by_type(uint voxel_type) {
+    if (voxel_type == VOXEL_TYPE_GRASS_LAND) {
+        return vec3(0.35, 0.57, 0.23);
+    } else if (voxel_type == VOXEL_TYPE_LEAF) {
+        return vec3(0.1, 0.5, 0.25);
+    } else if (voxel_type == VOXEL_TYPE_CHUNK) {
+        return vec3(0.65, 0.4, 0.27);
+    }
+    // empty voxel shouldn't be rendered at all
+    return vec3(0.0);
+}
 
 #endif // VOXEL_TYPE_GLSL
