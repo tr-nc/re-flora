7fe3bb7b0177e5c626922456b1e831285dbfbe6e
danny <ruitian0716@gmail.com>
2025-05-20 21:54:48 +0800

    resource loading of common blue noises

--- src/tracer/resources.rs
+++ src/tracer/resources.rs
@@ -1,5 +1,5 @@
 use crate::{
-    util::get_project_root,
+    util::{get_project_root, Timer},
     vkn::{
         Allocator, Buffer, BufferUsage, Device, ImageDesc, ShaderModule, Texture, VulkanContext,
     },
@@ -12,6 +12,9 @@ pub struct TracerResources {
     pub env_info: Buffer,
     pub shader_write_tex: Texture,
 
+    pub scalar_bn: Texture,
+    pub vec2_bn: Texture,
+    pub vec3_bn: Texture,
     pub weighted_cosine_bn: Texture,
 }
 
@@ -57,14 +60,74 @@ impl TracerResources {
             [screen_extent[0], screen_extent[1], 1],
         );
 
-        let weighted_cosine_bn = Self::create_bn(&vulkan_ctx, allocator.clone());
+        let timer = Timer::new();
+        let scalar_bn = create_bn(
+            &vulkan_ctx,
+            allocator.clone(),
+            "scalar_2d_1d_1d/stbn_scalar_2Dx1Dx1D_128x128x64x1_",
+        );
+        let vec2_bn = create_bn(
+            &vulkan_ctx,
+            allocator.clone(),
+            "unitvec2_2d_1d/stbn_unitvec2_2Dx1D_128x128x64_",
+        );
+        let vec3_bn = create_bn(
+            &vulkan_ctx,
+            allocator.clone(),
+            "unitvec3_2d_1d/stbn_unitvec3_2Dx1D_128x128x64_",
+        );
+        let weighted_cosine_bn = create_bn(
+            &vulkan_ctx,
+            allocator.clone(),
+            "unitvec3_cosine_2d_1d/stbn_unitvec3_cosine_2Dx1D_128x128x64_",
+        );
+        log::debug!("Blue noise texture load time: {:?}", timer.elapsed());
 
-        Self {
+        return Self {
             gui_input,
             camera_info,
             env_info,
             shader_write_tex,
+
+            scalar_bn,
+            vec2_bn,
+            vec3_bn,
             weighted_cosine_bn,
+        };
+
+        fn create_bn(
+            vulkan_ctx: &VulkanContext,
+            allocator: Allocator,
+            relative_path: &str,
+        ) -> Texture {
+            const BLUE_NOISE_LEN: u32 = 64;
+
+            let img_desc = ImageDesc {
+                extent: [128, 128, 1],
+                array_len: BLUE_NOISE_LEN,
+                format: vk::Format::R8G8B8A8_UNORM,
+                usage: vk::ImageUsageFlags::STORAGE | vk::ImageUsageFlags::TRANSFER_DST,
+                initial_layout: vk::ImageLayout::UNDEFINED,
+                aspect: vk::ImageAspectFlags::COLOR,
+                ..Default::default()
+            };
+            let sam_desc = Default::default();
+            let tex = Texture::new(vulkan_ctx.device().clone(), allocator, &img_desc, &sam_desc);
+
+            let base_path = get_project_root() + "/texture/stbn/";
+            for i in 0..BLUE_NOISE_LEN {
+                let path = format!("{}{}{}.png", base_path, relative_path, i);
+                tex.get_image()
+                    .load_and_fill(
+                        &vulkan_ctx.get_general_queue(),
+                        vulkan_ctx.command_pool(),
+                        &path,
+                        i,
+                        Some(vk::ImageLayout::GENERAL),
+                    )
+                    .unwrap();
+            }
+            tex
         }
     }
 
@@ -91,40 +154,6 @@ impl TracerResources {
         };
         let sam_desc = Default::default();
         let tex = Texture::new(device, allocator, &tex_desc, &sam_desc);
-        tex
-    }
-
-    fn create_bn(vulkan_ctx: &VulkanContext, allocator: Allocator) -> Texture {
-        const BLUE_NOISE_LEN: u32 = 64;
-
-        let img_desc = ImageDesc {
-            extent: [128, 128, 1],
-            array_len: BLUE_NOISE_LEN,
-            format: vk::Format::R8G8B8A8_UNORM,
-            usage: vk::ImageUsageFlags::STORAGE | vk::ImageUsageFlags::TRANSFER_DST,
-            initial_layout: vk::ImageLayout::UNDEFINED,
-            aspect: vk::ImageAspectFlags::COLOR,
-            ..Default::default()
-        };
-        let sam_desc = Default::default();
-        let tex = Texture::new(vulkan_ctx.device().clone(), allocator, &img_desc, &sam_desc);
-
-        for i in 0..BLUE_NOISE_LEN {
-            let path = format!(
-                "{}/texture/stbn/unitvec3_cosine_2d_1d/stbn_unitvec3_cosine_2Dx1D_128x128x64_{}.png",
-                get_project_root(),
-                i
-            );
-            tex.get_image()
-                .load_and_fill(
-                    &vulkan_ctx.get_general_queue(),
-                    vulkan_ctx.command_pool(),
-                    &path,
-                    i,
-                    Some(vk::ImageLayout::GENERAL),
-                )
-                .unwrap();
-        }
         tex
     }
 }
