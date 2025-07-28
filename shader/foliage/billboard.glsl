fd37bc77794ffb39fe0130faad78b3d0d40e8e8b
danny <ruitian0716@gmail.com>
2025-07-28 23:08:56 +0800

    refactor: enhance billboard vertex position calculation in foliage shaders for volumetric consistency

--- shader/foliage/billboard.glsl
+++ shader/foliage/billboard.glsl
@@ -3,8 +3,21 @@
 
 vec3 get_vert_pos_with_billboard(mat4 view_mat, vec3 voxel_pos, uvec3 vert_offset_in_vox,
                                  float scaling_factor) {
-    vec3 mapped_vert_offset_in_vox = vec3(vert_offset_in_vox) - 0.5;
+    // after this, the vert_offset_in_vox is in the range of [-0.5, 0.5] for each component
+    vec2 mapped_vert_offset_in_vox = vec2(vert_offset_in_vox.xy) - 0.5;
+
     mapped_vert_offset_in_vox *= scaling_factor;
+
+    // this constant is an approximation of sqrt(3/2) ≈ 1.225.
+    // for a cube with side length A, its expected (average) projected area over all
+    // possible random orientations is (3/2) * A^2. A square with this same area
+    // would have a side length of sqrt(3/2) * A.
+    // this scaling factor ensures that the 2D billboard's area statistically
+    // matches the projected area of the 3D voxel it represents, making it appear
+    // more volumetrically consistent from any viewing angle.
+    const float multiplier = 1.225;
+
+    mapped_vert_offset_in_vox *= multiplier;
 
     vec3 right = normalize(vec3(view_mat[0][0], view_mat[1][0], view_mat[2][0]));
     vec3 up    = normalize(vec3(view_mat[0][1], view_mat[1][1], view_mat[2][1]));
