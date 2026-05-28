c318730c6d3e56e729665a561ac2794af71b5598
ruitian <ruitian.y@bytedance.com>
2026-05-28 17:36:00 +0800

    clarify flora wind constants

--- shader/include/flora_registry.glsl
+++ shader/include/flora_registry.glsl
@@ -7,6 +7,7 @@ const uint FLORA_SPECIES_TALL_GRASS  = 0;
 const uint FLORA_SPECIES_SHORT_GRASS = 1;
 const uint FLORA_SPECIES_LAVENDER    = 2;
 const uint FLORA_SPECIES_EMBER_BLOOM = 3;
+// Render-only tree leaf instance type; not included in FLORA_SPECIES_COUNT.
 const uint FLORA_SPECIES_TREE_LEAF   = 4;
 
 #endif // FLORA_REGISTRY_GLSL
